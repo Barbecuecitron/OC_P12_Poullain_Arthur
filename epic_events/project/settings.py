1172e75b9aaebb226cc7b18b962ef39ecefa55c4
BoiluTheGod <arthur.poullain@gmail.com>
2022-07-03 14:38:35 +0200

    fixed auth session confusion

--- epic_events/project/settings.py
+++ epic_events/project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
-    # "dj_rest_auth",
     "user_management",
     "apps.contracts",
     "apps.events",
